1aec780eb126d4f6b6aa3aabb990c4d2ff3a7476
rukamasme3011 <rukamasme3011@gmail.com>
2024-04-10 00:28:04 +0200

    arreglado_login

--- apiRSCalendar/apiRSCalendar/obj/Release/net8.0/apiRSCalendar.AssemblyInfo.cs
+++ apiRSCalendar/apiRSCalendar/obj/Release/net8.0/apiRSCalendar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apiRSCalendar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46ee229862512fd270be767ff898dcab96fdc41")]
 [assembly: System.Reflection.AssemblyProductAttribute("apiRSCalendar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apiRSCalendar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
